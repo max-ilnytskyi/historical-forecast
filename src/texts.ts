fb4b354f6b0052e0d34905ea0346c1e05cd38fe8
Max Ilnitskyi <masimo4work@gmail.com>
2024-10-03 14:19:52 +0300

    Add metadata, remove fonts

--- src/texts.ts
+++ src/texts.ts
@@ -5,6 +5,10 @@ export const words = {
 
 export const strings = {
   temperatureInJerusalemC: 'Temperature in Jerusalem (°C)',
+  timeSeriesDataVisualizationFor:
+    'Time-Series Data Visualization for temperature in Jerusalem (°C)',
+  exploreInteractiveTimeSeriesVisualizationsOf:
+    'Explore interactive time-series visualizations of daily maximum and minimum temperatures in Jerusalem (°C) for the selected period. Analyze trends and gain insights into temperature changes over time.',
 } as const;
 
 export const errors = {
